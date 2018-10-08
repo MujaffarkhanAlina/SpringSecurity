1fed3bd9eb5d99a7f0d0c7938af793524692d76f
MujaffarKhanAlina <mujaffar.khan08@gmail.com>
2018-10-07 23:01:53 -0400

    six change

--- eureka/src/main/java/com/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -11,6 +11,7 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		
 		
+		System.out.println("branch tag to mater six chnage......");
 		System.out.println("branch tag to mater Five chnage......");
 		System.out.println("branch tag to mater branch......");
 		System.out.println("branch tag to mater branch......");
